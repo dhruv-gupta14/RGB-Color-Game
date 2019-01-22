effc32ae558ce0fac25f20fece963bbf8b21bdd0
dhruv-gupta14 <dhruvgupta1498@gmail.com>
2019-01-22 19:22:15 +0530

    rgb game

---
+++ rgb.js
@@ -0,0 +1,111 @@
+var numSquares=6;
+var colors=generateRandomColors(numSquares);
+var squares=document.querySelectorAll('.square');
+var colorDisplay=document.getElementById('colorDisplay');
+var pickedColor=pickColor();
+var messageDisplay=document.querySelector('#message');
+var resetButton=document.querySelector('#reset');
+var h1=document.querySelector('#h1');
+var easybtn=document.querySelector('#easybtn');
+var hardbtn=document.querySelector('#hardbtn');
+
+easybtn.addEventListener('click',function(){
+	numSquares=3;
+	  easybtn.classList.add('selected');  
+	  hardbtn.classList.remove('selected');
+	  colors=generateRandomColors(numSquares);
+	  pickedColor=pickColor();
+	  colorDisplay.textContent=pickedColor;
+	  for(var i=0;i<squares.length;i++){
+	  	if(colors[i]){
+	  		squares[i].style.backgroundColor=colors[i];
+	  	}
+	  	else{
+	  		squares[i].style.display='none';
+	  	}
+	  }  
+});
+
+hardbtn.addEventListener('click',function(){
+	numSquares=6;
+    easybtn.classList.remove('selected');  
+	  hardbtn.classList.add('selected');
+	  colors=generateRandomColors(numSquares);
+	  pickedColor=pickColor();
+	  colorDisplay.textContent=pickedColor;
+	  for(var i=0;i<squares.length;i++){
+	  	
+	  		squares[i].style.backgroundColor=colors[i];
+	  	
+	  	
+	  		squares[i].style.display='block';
+	  	
+	  }  
+});
+
+resetButton.addEventListener('click',function(){
+	colors=generateRandomColors(numSquares);
+	pickedColor=pickColor();
+	colorDisplay.textContent=pickedColor;
+	messageDisplay.textContent="";
+	this.textContent="NEW COLORS!!";
+	for(var i=0;i<squares.length;i++)
+	{
+		squares[i].style.backgroundColor=colors[i];
+	}
+	h1.style.backgroundColor="steelblue";
+	messageDisplay.style.color='white';
+});
+
+colorDisplay.textContent=pickedColor;
+
+for(var i=0;i<colors.length;i++)
+{
+	squares[i].style.backgroundColor=colors[i];
+
+	squares[i].addEventListener('click',function(){
+         var clickedColor=this.style.backgroundColor;
+         if (clickedColor==pickedColor) {
+         	messageDisplay.textContent='Correct!!!!';
+         	changeColor(clickedColor);
+         	resetButton.textContent='Play Again';
+         	h1.style.backgroundColor=clickedColor;
+         }
+         else{
+         	this.style.backgroundColor='#232323';
+         	messageDisplay.textContent='Try Again.....';
+         }
+         /*console.log('clickedColor,pickedColor');*/
+	});
+}
+
+function changeColor(color){
+	for(var i=0;i<squares.length;i++){
+		squares[i].style.backgroundColor=color;
+	}
+}
+
+function pickColor(){
+	var random=Math.floor(Math.random()*colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num){
+	var arr=[];
+
+for(var i=0;i<num;i++){
+   arr.push(randomColors());
+}
+
+	return arr;
+}
+
+
+function randomColors(){
+	var r=Math.floor(Math.random()*256);
+	var b=Math.floor(Math.random()*256);
+	var g=Math.floor(Math.random()*256);
+
+	return 'rgb(' + r +', ' + g + ', ' + b + ')';
+
+}
